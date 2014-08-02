c80bb1943b784848582245a97ca4bec75916ea59
Matt Fenwick <mfenwick100@gmail.com>
2014-08-02 19:02:41 -0400

    get it up and running again after the clojarse-js code was split off

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 'use strict';
 
-var FP = require('./lib/parser/full'),
-    D = require('./lib/astdumper'),
-    T = require('./lib/simpletree/checker'),
-    B = require('./lib/astbuilder'),
+var C = require('clojarse-js'),
+    dump = C.ast.dump,
+    T = require('./lib/checker'),
     fs = require('fs');
 
+var input = fs.readFileSync('/dev/stdin', {'encoding': 'utf8'}),
+    ast = C.parseAst(input),
+    output;
 
-var input = fs.readFileSync('/dev/stdin', {'encoding': 'utf8'});
+function formatError(e) {
+    return '    line ' + e[0] + ', column ' + e[1];
+}
 
-var parsed = FP.fullParse(input);
-var ast = parsed.fmap(B.build);
-
-ast.fmap(function(a) {
-    var out = T.default_traverse(a),
+if ( ast.status === 'success' ) {
+    var out = T.default_traverse(ast.value),
         state = out[0],
         log = out[1]; 
     console.log('state -- ' + JSON.stringify(state, null, 2));
     log._issues.map(function(e) { console.log(JSON.stringify(e)); });
     console.log('node types -- ' + JSON.stringify(log.node_types, null, 2));
-    console.log('symbol use -- ' + JSON.stringify(log._symbol_use, null, 2));
-    console.log();
-});
-
-// var output = JSON.stringify(parsed, null, 2) + JSON.stringify(ast, null, 2) + ast.fmap(A.dump).value;
-// var output = JSON.stringify(ast, null, 2) + '\n' + ast.fmap(A.dump).value;
-var output = ast.fmap(D.dump).mapError(JSON.stringify).value;
-// var output = ast.mapError(JSON.stringify).value;
+    console.log('symbol use -- ' + JSON.stringify(log._symbol_use, null, 2) + "\n");
+    output = dump(ast.value);
+} else {
+    try {
+        output = ['error: '].concat(ast.value.map(formatError)).join('\n');
+    } catch (e) { // one way to trigger this branch: input is "\oopsie"
+        console.log(JSON.stringify(ast.value));
+        throw e;
+    }
+}
 
 /*
 fs.writeFile('output', 
