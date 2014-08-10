c9e1f08a8f8255678c216027197569d42f6a2ca5
Matt Fenwick <mfenwick100@gmail.com>
2014-08-10 07:17:51 -0400

    get code up and running with examples

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var input = fs.readFileSync('/dev/stdin', {'encoding': 'utf8'}),
     ast = C.parseAst(input),
     output = M.standard(ast.value);
 
-console.log('output: ' + JSON.stringify(output));
+//console.log('output: ' + JSON.stringify(output));
 process.stdout.write((typeof output === 'string' ? 
                       output :
                       JSON.stringify(output, null, 2))   + "\n");
