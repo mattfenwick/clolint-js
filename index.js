2c903da8cec5aee0e5be27aab90e6af7e2386578
Matt Fenwick <mfenwick100@gmail.com>
2014-08-10 15:35:21 -0400

    get special form syntax checks up and running again

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var M = require('./lib/main'),
     C = require('clojarse-js'),
     fs = require('fs');
 
-var input = fs.readFileSync('/dev/stdin', {'encoding': 'utf8'}),
-    ast = C.parseAst(input),
-    output = M.standard(ast.value);
+var input   = fs.readFileSync('/dev/stdin', {'encoding': 'utf8'}),
+    ast     = C.parseAst(input),
+    output  = M.standard(ast.value);
 
 //console.log('output: ' + JSON.stringify(output));
 process.stdout.write((typeof output === 'string' ? 
